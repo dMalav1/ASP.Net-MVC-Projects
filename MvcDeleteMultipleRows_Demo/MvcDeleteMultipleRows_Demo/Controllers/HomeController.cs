79aca41bc0b40ef44db2939f78401e024985846f
Dheerendra Malav <dheerendra.malav@outlook.com>
2021-01-07 09:30:47 +0530

    Adding Sol File

---
+++ MvcDeleteMultipleRows_Demo/MvcDeleteMultipleRows_Demo/Controllers/HomeController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MvcDeleteMultipleRows_Demo.Models;
+
+namespace MvcDeleteMultipleRows_Demo.Controllers
+{
+    public class HomeController : Controller
+    {
+        private DBModel db = new DBModel();
+
+        [HttpGet]
+        public ActionResult Index()
+        {
+            ViewBag.ListEmployee = this.db.Employees.ToList();
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Index(FormCollection formCollection)
+        {
+            string[] ids = formCollection["ID"].Split(new char[] { ',' });
+            foreach (string id in ids)
+            {
+                var employee = this.db.Employees.Find(int.Parse(id));
+                this.db.Employees.Remove(employee);
+                this.db.SaveChanges();
+            }
+            return RedirectToAction("Index");
+        }
+    }
+}
